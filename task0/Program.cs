d5345cdfc5b0c35a80bb35aa7d4c425270bff29e
Nikolay <meta1mang@mail.ru>
2022-12-02 11:08:56 +0300

    Задача 1 с преподавателем Квадрат числа

---
+++ task0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает
+// число и выдает его квадрат (число умноженное
+// на само себя).
+// Например:
+// ● 4 -> 16
+// ● -3 -> 9
+// ● -7 -> 49 
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(number*number);
+
+int res = number*number;
+Console.WriteLine("Квадрат числа: " + res);
+
+
+
